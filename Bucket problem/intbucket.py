07882c153aa5d2347dd8ff35621a19692b7bb22f
tiendatngcs <44583359+tiendatngcs@users.noreply.github.com>
2019-04-20 22:46:41 -0500

    Add files via upload

--- Bucket problem/intbucket.py
+++ Bucket problem/intbucket.py
@@ -7,13 +7,17 @@ from sys import argv
 
 
 def recur (bucket, target, count):
+	bucket = listsimplifier(bucket)
+	#if there is no bucket initially
+	if len(bucket) == 0:
+		if target[0] == 0: return True
+		else: return False
 	#if recursion depth exceed 100 then return false
 	if count > 100: return False
 	#if there is only one bucket
-	bucket = listsimplifier(bucket)
 	if len (bucket) == 1:
 		for i in target:
-			if i % bucket[0] != 0: return False 
+			if i % bucket[0] != 0 or i * bucket[0] <= 0: return False 
 	#for every element in bucket if any of them is divisible to target then return true
 	for i in target:
 		for j in bucket:
@@ -31,15 +35,17 @@ def recur (bucket, target, count):
 
 def listsimplifier (mylist):
 	mylist.sort()
-	#exclude any multiple of all elements
-	i = 0
-	while i < len(mylist)-1:
-		j = i + 1
-		while j <= len (mylist)-1:
-			if mylist[j] % mylist [i] == 0: 
-				del mylist [j]
-			else: j = j + 1
-		i = i +1
+	while 0 in mylist: mylist.remove(0)
+	#exclude any same-sign multiple of all elements
+	if len(mylist) != 0:
+		i = 0
+		while i < len(mylist)-1:
+			j = i + 1
+			while j <= len (mylist)-1:
+				if (mylist[j] % mylist [i] == 0 and mylist[j] * mylist[i] > 0): 
+					del mylist [j]
+				else: j = j + 1
+			i = i +1
 	return mylist
 
 
@@ -47,5 +53,7 @@ def listsimplifier (mylist):
 script =  argv[0]
 target = list(map(int, argv[1:2]))
 bucket = list(map(int,argv[2:]))
-#find smallest value in bucket
+if target[0] < 0:
+	target = [-x for x in target]
+	bucket = [-x for x in bucket]
 print (recur (bucket, target, 0))
